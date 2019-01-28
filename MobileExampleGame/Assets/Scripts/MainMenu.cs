8082c52f433add9455e24d4b8878794285a47504
lethosoict2016 <thomlew1012@gmail.com>
2019-01-28 18:06:57 +0700

    Video 4.1
    Slerp MainCamera

--- MobileExampleGame/Assets/Scripts/MainMenu.cs
+++ MobileExampleGame/Assets/Scripts/MainMenu.cs
@@ -9,8 +9,15 @@ public class MainMenu : MonoBehaviour
     public GameObject levelButtonPrefabs;
     public GameObject levelButtonContaner;
 
+    private Transform cameraTransform;
+    private Transform cameraDesiredLookAt;
+    private const float CAMERA_TRANSITION_SPEED = 3.0f;
+
     private void Start()
     {
+        cameraTransform = Camera.main.transform;
+
+
         Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
         foreach(Sprite thumbnail in thumbnails)
         {
@@ -23,10 +30,25 @@ public class MainMenu : MonoBehaviour
         }
     }
 
+
+    private void Update()
+    {
+     if(cameraDesiredLookAt != null)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, CAMERA_TRANSITION_SPEED * Time.deltaTime);
+        }   
+    }
+
     private void LoadLevel(string sceneName)
     {
         Debug.Log(sceneName);
        // SceneManager.LoadScene(sceneName);
+    }
+
+    public void LookAt(Transform menuTransform)
+    {
+        //Camera.main.transform.LookAt(menuTransform.position);
+        cameraDesiredLookAt = menuTransform;
     }
 
 }
