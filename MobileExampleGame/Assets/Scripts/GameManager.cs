74e74211e3079ee6f92bf09614630e6aacfa7360
lethosoict2016 <thomlew1012@gmail.com>
2019-01-29 17:36:02 +0700

    Video 7.1
    DontDestroyonLoad. PlayerPrefabs GetIn, SetIn

---
+++ MobileExampleGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; }}
+
+    public int currentSkinIndex;
+    public int currency;
+    public int skinAvailability;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        if (PlayerPrefs.HasKey("CurrentSkin"))
+        {
+            currentSkinIndex = PlayerPrefs.GetInt("CurrentSkin");
+            currency = PlayerPrefs.GetInt("Currency");
+            skinAvailability = PlayerPrefs.GetInt("SkinAvailability");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("CurrentSkin", currentSkinIndex);
+            PlayerPrefs.SetInt("Currency", currency);
+            PlayerPrefs.SetInt("SkinAvailability", skinAvailability);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
