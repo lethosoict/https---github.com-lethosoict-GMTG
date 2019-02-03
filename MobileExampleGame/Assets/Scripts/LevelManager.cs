a616241792f7b374b8d8a47ca9a66e3a7852d9c2
DESKTOP-FNHVELF\Phoebus Gannicus <letho.unity@gmail.com>
2019-02-04 00:43:46 +0700

    End Video 8
    Add Player, Add Palne

--- MobileExampleGame/Assets/Scripts/LevelManager.cs
+++ MobileExampleGame/Assets/Scripts/LevelManager.cs
@@ -6,6 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+
     public void ToMenu()
     {
         SceneManager.LoadScene("MainMenu");
