0d092fbba10585296280bf608fbc0b54d735bed4
DESKTOP-FNHVELF\Phoebus Gannicus <letho.unity@gmail.com>
2019-02-05 02:01:09 +0700

    Video 9. Min 11.24 - Slide Camera

---
+++ MobileExampleGame/Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+
+    private Vector3 offset;
+
+    private float distance = 5.0f;
+    private float yOffset = 3.5f;
+
+    private void Start()
+    {
+        offset = new Vector3(0, yOffset, -1f * distance);
+    }
+
+    private void Update()
+    {
+        transform.position = lookAt.position + offset;
+    }
+
+    public void SlideCamera(bool left)
+    {
+        if (left)
+            offset = Quaternion.Euler(0, 90, 0) * offset;
+        else
+            offset = Quaternion.Euler(0, -90, 0) * offset;
+    }
+}
