f1a0ed3e2f1a9a4103377d85086d04c131696b77
DESKTOP-FNHVELF\Phoebus Gannicus <letho.unity@gmail.com>
2019-02-04 23:48:57 +0700

    Movement

--- MobileExampleGame/Assets/Scripts/Motor.cs
+++ MobileExampleGame/Assets/Scripts/Motor.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Motor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 5.0f;
+    public float drag = 0.5f;
+    public float terminalRotationSpeed = 25.0f;
+
+    private Rigidbody controller;
+
+    private void Start()
     {
-        
+        controller = GetComponent<Rigidbody>();
+        controller.maxAngularVelocity = terminalRotationSpeed;
+        controller.drag = drag;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 dir = Vector3.zero;
+
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+
+        if (dir.magnitude > 1)
+            dir.Normalize();
+
+        controller.AddForce(dir * moveSpeed);
     }
 }
