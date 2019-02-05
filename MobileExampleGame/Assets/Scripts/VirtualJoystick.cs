7127d1a62ee42171950469dadd023ec6c973ae24
DESKTOP-FNHVELF\Phoebus Gannicus <letho.unity@gmail.com>
2019-02-05 14:34:58 +0700

    Video 10.1 - Create VirtualJoystick script

---
+++ MobileExampleGame/Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public virtual void OnDrag(PointerEventData ped)
+    {
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+
+    }
+}
