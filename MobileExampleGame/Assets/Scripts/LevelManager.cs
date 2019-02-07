8c54fb71a0f65b498717dca2d1d1ec6f5a7a7c20
DESKTOP-FNHVELF\Phoebus Gannicus <letho.unity@gmail.com>
2019-02-07 13:16:27 +0700

    Video 13 - Min 13. SetString by PlayerPrefs. Save currency and Scene

--- MobileExampleGame/Assets/Scripts/LevelManager.cs
+++ MobileExampleGame/Assets/Scripts/LevelManager.cs
@@ -6,11 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    private static LevelManager instance;
+    public static LevelManager Instance { get { return instance;} }
+
     public GameObject pauseMenu;
+
+    private float startTime;
+    public float silverTime;
+    public float goldTime;
 
     private void Start()
     {
+        instance = this;
         pauseMenu.SetActive(false);
+        startTime = Time.time;
     }
 
     public void TogglePauseMenu()
@@ -21,5 +30,33 @@ public class LevelManager : MonoBehaviour
     public void ToMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Victory()
+    {
+        // Debug.Log("Victory");
+        float duration = Time.time - startTime;
+        if(duration < goldTime)
+        {
+            GameManager.Instance.currency += 50;
+        }
+        else if(duration < silverTime)
+        {
+            GameManager.Instance.currency += 25;
+        }
+        else
+        {
+            GameManager.Instance.currency += 10;
+        }
+        GameManager.Instance.Save();
+
+        string saveString = "";
+        saveString += duration.ToString();
+        saveString += '&';
+        saveString += silverTime.ToString();
+        saveString += '&';
+        saveString += goldTime.ToString();
+        PlayerPrefs.SetString(SceneManager.GetActiveScene().name, saveString);
+        Debug.Log(SceneManager.GetActiveScene().name);
     }
 }
--- MobileExampleGame/Assets/Scripts/WinBox.cs
+++ MobileExampleGame/Assets/Scripts/WinBox.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class WinBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider col)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(col.tag == "Player")
+        {
+            // Victory
+            LevelManager.Instance.Victory();
+        }
     }
 }
